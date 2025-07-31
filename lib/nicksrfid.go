f997bc0073105bf4828b96feafe27b2955d7f809
trendsuggest.com <173702321+trendsuggest-com@users.noreply.github.com>
2025-07-31 13:52:48 +0530

    Update nicksrfid.go

--- lib/nicksrfid.go
+++ lib/nicksrfid.go
@@ -1,1 +1,10 @@
+// Package rfid provides structured tracing logs for Go routines.
+package rfid
+
+import "fmt"
+
+func RFIDLog(queue, funcID, workerID, format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	fmt.Printf("[%s][%s][%s] %s\n", queue, funcID, workerID, msg)
+}
 
